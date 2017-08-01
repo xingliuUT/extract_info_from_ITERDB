79fa23f5a0819096c66628ffd16e5ccccc4bf903
Xing Liu <xingliu@utexas.edu>
2017-08-01 17:48:03 -0500

    Compute: chi = Power_input / area / dTedx / ne

--- TestDrive_iterdb.py
+++ TestDrive_iterdb.py
@@ -4,6 +4,7 @@ import sys
 from interp import *
 from finite_differences_x import *
 from read_EFIT import *
+from calc_volume_from_EFIT import *
 
 #rhot_te1, te1, ti1, ne1, ni1, nb1, vrot1 = read_iterdb_file(sys.argv[1])
 ITERDBdict = read_iterdb_x(sys.argv[1])
@@ -15,11 +16,25 @@ te_uniR = interp(ITERDBdict['rhot_te'], ITERDBdict['te'], rhot_uniR)
 d_te_d_R = - first_derivative(te_uniR, uni_R)
 ne_uniR = interp(ITERDBdict['rhot_ne'], ITERDBdict['ne'], rhot_uniR)
 
+Qheating_MW = 1.
+ntheta = 512
+area_m2 = surfaceArea(EFITdict, ntheta)
+# convert eV into J: multiply by e
+charge_e = 1.6E-19
+chi_uniR = Qheating_MW * 1.E6 / area_m2 / d_te_d_R / charge_e / ne_uniR
+
 if 1 == 1:
+    #plt.plot(rhot_uniR, chi_uniR, '.', label = 'chi')
+    plt.semilogy(rhot_uniR, chi_uniR, '.', label = 'chi')
+    plt.axis([0.85, 1., 0.01, 1.])
+    plt.xlabel('rhot')
+    plt.legend()
+    plt.show()
+if 1 == 0:
     plt.plot(rhot_uniR, d_te_d_R, '.', label = 'd Te / d R')
     plt.legend()
     plt.show()
-if 1 == 1:
+if 1 == 0:
     plt.plot(rhot_uniR, ne_uniR, '.', label = 'ne')
     plt.xlabel('rhot')
     plt.legend()
