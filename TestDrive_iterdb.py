e8c1235c3c821935f362eededd43eba38ffe4b04
Xing Liu <xingliu@utexas.edu>
2017-08-01 17:18:11 -0500

    Add: convert ne on uniform grid of R

--- TestDrive_iterdb.py
+++ TestDrive_iterdb.py
@@ -13,9 +13,15 @@ uni_R = np.linspace(EFITdict['R'][0], EFITdict['R'][-1], len(EFITdict['R'])*10)
 rhot_uniR = interp(EFITdict['R'], EFITdict['rhotn'], uni_R)
 te_uniR = interp(ITERDBdict['rhot_te'], ITERDBdict['te'], rhot_uniR)
 d_te_d_R = - first_derivative(te_uniR, uni_R)
+ne_uniR = interp(ITERDBdict['rhot_ne'], ITERDBdict['ne'], rhot_uniR)
 
 if 1 == 1:
     plt.plot(rhot_uniR, d_te_d_R, '.', label = 'd Te / d R')
+    plt.legend()
+    plt.show()
+if 1 == 1:
+    plt.plot(rhot_uniR, ne_uniR, '.', label = 'ne')
+    plt.xlabel('rhot')
     plt.legend()
     plt.show()
 if 1 == 0:
