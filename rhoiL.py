1f29b251b38c3d46ea50ecf0bed158d374b92abe
Xing Liu <xingliu@utexas.edu>
2017-11-13 10:39:08 -0600

    feat: using Ti for computing rho_i

--- rhoiL.py
+++ rhoiL.py
@@ -6,6 +6,7 @@ from interp import *
 import matplotlib.pyplot as plt
 import numpy as np
 from read_EFIT import *
+import sys
 
 def dProfdR(ITERDBdict, EFITdict, profile, rhotLeft, rhotRight):
     rhot0 = EFITdict['rhotn']
@@ -48,8 +49,10 @@ def dProfdrhot(ITERDBdict, EFITdict, profile, rhotLeft, rhotRight):
 
     return uni_rhot, prof_unirhot, profPrime_unirhot
 
-iterdbFileName = "negOmTorDIIID98889.iterdb"
-efitFileName = "g098889.04530"
+#iterdbFileName = "negOmTorDIIID98889.iterdb"
+#efitFileName = "g098889.04530"
+iterdbFileName = sys.argv[1]
+efitFileName = sys.argv[2]
 EFITdict = read_EFIT(efitFileName)
 #rhot0 = EFITdict['rhotn'][100:]
 #R0 = EFITdict['R'][100:]
@@ -73,20 +76,22 @@ if 1 == 1:
         rhot2, te, tprime_e = dProfdR(ITERDBdict, EFITdict, 'te', 0.9, 1.0)
         rhot2, ti, tprime_i = dProfdR(ITERDBdict, EFITdict, 'ti', 0.9, 1.0)
         rhot2, ni, fprime_i = dProfdR(ITERDBdict, EFITdict, 'ni', 0.9, 1.0)
-        rhoi_m = 1.02*np.sqrt(mref)*np.sqrt(te)/Bref_Gauss
+        #rhoi_m = 1.02*np.sqrt(mref)*np.sqrt(te)/Bref_Gauss
+        rhoi_m = 1.02*np.sqrt(mref)*np.sqrt(ti)/Bref_Gauss
         result2 = rhoi_m * (fprime_i + tprime_i)
     if 1 == 1:
         rhot1, te, tprime_e = dProfdrhot(ITERDBdict, EFITdict, 'te', 0.9, 1.0)
         rhot1, ti, tprime_i = dProfdrhot(ITERDBdict, EFITdict, 'ti', 0.9, 1.0)
         rhot1, ni, fprime_i = dProfdrhot(ITERDBdict, EFITdict, 'ni', 0.9, 1.0)
-
-        rhoi_m = 1.02*np.sqrt(mref)*np.sqrt(te)/Bref_Gauss
+        #rhoi_m = 1.02*np.sqrt(mref)*np.sqrt(te)/Bref_Gauss
+        rhoi_m = 1.02*np.sqrt(mref)*np.sqrt(ti)/Bref_Gauss
         result1 = rhoi_m * (fprime_i + tprime_i) / Lref_m
 
     plt.plot(rhot1, result1, label = 'derivative wrt sqrt(psi_tor)')
     plt.plot(rhot2, result2, label = 'derivative wrt major radius (m)')
     plt.xlabel('rhot')
     plt.legend(loc = 2)
-    plt.axis([0.9,1.0,0.,0.35])
-    plt.title('rho_i / L for DIIID' + efitFileName[2:7])
-    plt.show()
+    plt.axis([0.9,1.0,0.,0.5])
+    plt.title('rho_i / L_i for DIIID' + efitFileName[1:7])
+#    plt.show()
+    plt.savefig('Callen_rhoiLi.pdf', format = 'pdf')
