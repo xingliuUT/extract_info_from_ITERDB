d92f38097ce68df8ad88ed45863efbab1528af9e
Xing Liu <xingliu@utexas.edu>
2017-11-30 22:10:12 -0600

    feat: convert ti, ni on uniform R grid

--- Er_gradPi.py
+++ Er_gradPi.py
@@ -11,3 +11,16 @@ efitFileName = sys.argv[2]
 EFITdict = read_EFIT(efitFileName)
 ITERDBdict = read_iterdb_x(iterdbFileName)
 
+Rgrid = EFITdict['R']
+
+uniR = np.linspace(Rgrid[0], Rgrid[-1], len(Rgrid))
+rhot_uniR = interp(Rgrid, EFITdict['rhotn'], uniR)
+
+# ti in ITERDB file is in the unit of eV
+ti_uniR = interp(ITERDBdict['rhot_ti'], ITERDBdict['ti'], rhot_uniR)
+ni_uniR = interp(ITERDBdict['rhot_ni'], ITERDBdict['ni'], rhot_uniR)
+pi_uniR = ti_uniR * ni_uniR
+
+gradPioverNie = first_derivative(pi_uniR, uniR) / ni_uniR
+
+
