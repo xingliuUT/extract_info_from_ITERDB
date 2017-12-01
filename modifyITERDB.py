e9acd226f81f17232c26b8646ca57f8aed589530
Xing Liu <xingliu@utexas.edu>
2017-11-30 21:47:40 -0600

    feat: shift Er left/right

--- modifyITERDB.py
+++ modifyITERDB.py
@@ -10,14 +10,18 @@ import numpy as np
 
 ITERDBdict = read_iterdb_x(sys.argv[1])
 EFITdict = read_EFIT(sys.argv[2])
-file_out_base = 'new' + 'DIIID' 
-base_number = '98889'
-time_str = '4530'
-psipn = interp(EFITdict['rhotn'], EFITdict['psipn'], ITERDBdict['rhot_te'])
-plt.plot(EFITdict['rhotn'], EFITdict['psipn'], label = 'old')
-plt.plot(ITERDBdict['rhot_te'], psipn, label = 'new')
+vrot0 = ITERDBdict['vrot']
+rhot_vrot0 = ITERDBdict['rhot_vrot']
+shift = 0.005
+vrot_shifted = interp(rhot_vrot0 - shift, vrot0, rhot_vrot0)
+plt.plot(rhot_vrot0, vrot0, label = 'before')
+plt.plot(ITERDBdict['rhot_te'], vrot_shifted, label = 'new')
 plt.legend()
 plt.show()
+file_out_base = 'efit_DIIID153764_' + 'off7Zp2bsx11_120_exp_vrot_left'
+base_number = '153764'
+time_str = '0.0'
+psipn = interp(EFITdict['rhotn'], EFITdict['psipn'], ITERDBdict['rhot_te'])
 rhop = np.sqrt(abs(psipn))
 output_iterdb(ITERDBdict['rhot_te'], \
               rhop, \
@@ -28,5 +32,5 @@ output_iterdb(ITERDBdict['rhot_te'], \
               file_out_base+base_number, \
               base_number, \
               time_str, \
-              vrot=ITERDBdict['vrot'], \
+              vrot=vrot_shifted, \
               nimp=ITERDBdict['nz']*1.E-19)
