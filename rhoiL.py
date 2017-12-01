9d254e318c6553693cda87827600a36dfb7ac7ea
Xing Liu <xingliu@utexas.edu>
2017-11-30 18:00:28 -0600

    feat: mi/me (Lped/Ls)^2, Lped/R

--- rhoiL.py
+++ rhoiL.py
@@ -8,6 +8,41 @@ import numpy as np
 from read_EFIT import *
 import sys
 
+def dPressuredR(ITERDBdict, EFITdict, rhotLeft, rhotRight):
+    rhot0 = EFITdict['rhotn']
+    R0 = EFITdict['R']
+    Pres = EFITdict['Pres']
+    print(len(rhot0), len(R0), len(Pres))
+
+    leftInd = np.argmin(abs(rhot0 - rhotLeft))
+    rightInd = np.argmin(abs(rhot0 - rhotRight))
+
+    rhotProf = rhot0[leftInd: rightInd + 1]
+    prof = Pres[leftInd: rightInd + 1]
+    R = R0[leftInd: rightInd + 1]
+
+    uniR = np.linspace(R[0], R[-1], len(R))
+    rhot_uniR = interp(R, rhotProf, uniR)
+    prof_uniR = interp(R, prof, uniR)
+    if 1 == 0:
+        plt.plot(R0, rhot0, '+-', label = 'rhot0')
+        plt.plot(uniR, rhot_uniR, '+-', label = 'rhot_uniR')
+        plt.xlabel('R')
+        plt.ylabel('rhot')
+        plt.legend()
+        plt.show()
+
+        plt.plot(R0, Pres, '+-', label = 'Pres')
+        plt.plot(uniR, prof_uniR, '+-', label = 'prof_uniR')
+        plt.xlabel('R')
+        plt.ylabel('pressure')
+        plt.legend()
+        plt.show()
+
+    profPrime_uniR = - first_derivative(prof_uniR, uniR) / prof_uniR
+
+    return rhot_uniR, uniR, prof_uniR, profPrime_uniR
+
 def dProfdR(ITERDBdict, EFITdict, profile, rhotLeft, rhotRight):
     rhot0 = EFITdict['rhotn']
     R0 = EFITdict['R']
@@ -59,7 +94,7 @@ EFITdict = read_EFIT(efitFileName)
 
 Zave = 6.
 Bref_Gauss = abs(EFITdict['bcentr']) * 1.E04    #1.95451E04
-Lref_m = 0.76
+Lref_m = 0.764  #off7 = 0.7567 #Callen = 0.764, Diallo = 0.756
 mref = 2.
 
 ITERDBdict = read_iterdb_x(iterdbFileName)
@@ -70,8 +105,45 @@ ITERDBdict = read_iterdb_x(iterdbFileName)
 
 e = 1.6E-19
 
-
 if 1 == 1:
+    uni_rhot, shat, Ls = magneticShear(EFITdict)
+    rhot_uniR, uni_R, pres, dpresdR = dPressuredR(ITERDBdict, EFITdict, 0.9, 1.0)
+    Lped = 1. / dpresdR
+
+
+    if 1 == 1:
+        # convert Ls onto uniform R grid
+        Ls_uniR = interp(uni_rhot, Ls, rhot_uniR)
+        # convert Lped onto uniform rhot grid
+        Lped_unirhot = interp(rhot_uniR, Lped, uni_rhot)
+        if 1 == 0:
+            plt.plot(rhot_uniR, Lped, '+-')
+            plt.plot(uni_rhot, Lped_unirhot, '+-')
+            plt.ylabel('Lped')
+            plt.xlabel('rhot')
+            plt.xlim([0.9, 1.])
+            plt.show()
+
+        fig, ax = plt.subplots(figsize = (6, 4.5))
+        y1 = mref * 1836 * (Lped_unirhot / Ls)**2
+        ax.semilogy(uni_rhot, y1, 'o-', linewidth = 2., ms = 4., \
+        label = r'$\frac{m_i}{m_e} (\frac{L_{ped}}{L_s})^2$')
+        ax.set_xlabel(r'$\rho_{t}$', fontsize = 12)
+        ax.set_xlim([0.9, 1.])
+        ax.set_ylim([1E-5, 20.])
+        #ax.set_xlim([0.94, 1.])
+        #ax.set_ylim([5E-6, 10.])
+        ax.semilogy(rhot_uniR, Lped / uni_R, 'o-', linewidth = 2., \
+        ms = 4., label = r'$\frac{L_{ped}}{R}$')
+#        ax.set_title(efitFileName)
+        ax.legend(fontsize = 'large')
+        ax.tick_params(axis='both',which='major',labelsize=12,\
+        length=3,width=2,direction='out')
+        plt.savefig(filename = 'Callen_log_plot1.pdf', format = 'pdf')
+        #plt.savefig(filename = 'Diallo_off7_log_plot1.pdf', format = 'pdf')
+        #plt.show()
+
+if 1 == 0:
     if 1 == 1:
         rhot2, te, tprime_e = dProfdR(ITERDBdict, EFITdict, 'te', 0.9, 1.0)
         rhot2, ti, tprime_i = dProfdR(ITERDBdict, EFITdict, 'ti', 0.9, 1.0)
@@ -93,5 +165,6 @@ if 1 == 1:
     plt.legend(loc = 2)
     plt.axis([0.9,1.0,0.,0.5])
     plt.title('rho_i / L_i for DIIID' + efitFileName[1:7])
-#    plt.show()
-    plt.savefig('Callen_rhoiLi.pdf', format = 'pdf')
+#    plt.title('rho_i / L_i for DIIID off7')
+    plt.show()
+#    plt.savefig('Callen_rhoiLi.pdf', format = 'pdf')
